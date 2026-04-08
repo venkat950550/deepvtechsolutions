4bd3917fee2871afcc2232f09d44c4f2449655ec
venkat950550 <ibm.145138601@gmail.com>
2026-04-08 10:36:55 -0500

    config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1];
-const base = process.env.GITHUB_ACTIONS === 'true' && repoName ? `/${repoName}/` : '/';
 
 export default defineConfig({
   plugins: [react()],
-  base,
+  base: '/',
 });
